5b734ad75ce7446fb351cc0423ccf86d6d714975
Kashmir Shrestha <cthakashmir999@gmail.com>
2026-01-03 16:53:48 +0545

    Tensor v3.0 --application closed

--- src/components/Apply/Apply.jsx
+++ src/components/Apply/Apply.jsx
@@ -7,7 +7,7 @@ export default function Apply() {
   useEffect(() => {
     window.scrollTo(0, 0)
   }, [])
-  const applicationClosed = false;
+  const applicationClosed = true;
   return (
     <>
       {applicationClosed ? (
